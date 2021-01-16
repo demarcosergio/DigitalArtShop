cc78580dae3200d5a78b53757cb237aad56bb46e
sdemarco <sdemarco@pagustech.com>
2021-01-15 21:44:20 -0300

    Updates

---
+++ precache-manifest.3aaddc7b26e77b9fb4b1aadc507127a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd575d3602ea65d96db",
+    "url": "/DigitalArtShop/static/css/main.3aab5216.chunk.css"
+  },
+  {
+    "revision": "6cd575d3602ea65d96db",
+    "url": "/DigitalArtShop/static/js/main.6cd575d3.chunk.js"
+  },
+  {
+    "revision": "82bf1f28914ca1bb46dd",
+    "url": "/DigitalArtShop/static/js/runtime~main.82bf1f28.js"
+  },
+  {
+    "revision": "4c9e74fe04d9db5f872e",
+    "url": "/DigitalArtShop/static/js/2.4c9e74fe.chunk.js"
+  },
+  {
+    "revision": "e94f9ec79f00845b020a13c2e52fba3a",
+    "url": "/DigitalArtShop/index.html"
+  }
+];
